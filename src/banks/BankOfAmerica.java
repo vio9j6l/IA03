887b709178fe363e0ec395a29c20d055748e426e
vio9j6l <april951ss@gmail.om>
2020-01-30 09:00:36 -0500

    TODO - 01

--- src/banks/BankOfAmerica.java
+++ src/banks/BankOfAmerica.java
@@ -1,24 +1,37 @@
 package banks;
 
+import lionpay.BankActionStatus;
+import lionpay.IBank;
+
 // TODO 01 - Modify this class to implement IBank interface
 //         - MODIFY THE EXISTING METHODS TO FULFILL THE INTERFACE CONTRACT
 //         - DO NOT ADD NEW METHODS
 //         - Remember to user @Override when appropriate
-public class BankOfAmerica
+public class BankOfAmerica implements IBank
 {
   private double balance;
 
-  public String putMoneyIn(double amount) {
+  public BankActionStatus putMoneyIn(double amount) {
     balance += amount;
-    return "success";
+    return BankActionStatus.SUCCESS;
   }
 
-  public String withdraw(double amount) {
+  public double withdraw(double amount) {
     if (amount > balance) {
       return "err"; // insufficient funds
     }
     balance -= amount;
     return "success";
+  }
+
+  @Override
+  public BankActionStatus deposit(double amount) {
+    return null;
+  }
+
+  @Override
+  public double getBalance() {
+    return 0;
   }
 
   public double getCurrentBalance() {
